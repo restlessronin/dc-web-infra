8b8de61f61cdfc4b522835247270c0e9577a3758
restlessronin <88921269+restlessronin@users.noreply.github.com>
2024-05-17 00:10:07 +0530

    add explicit project

--- renew-reload.sh
+++ renew-reload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd /srv/docker/certbot && \
-    docker-compose -f docker-compose.yml run --rm certbot
+    docker-compose -p webinfra -f docker-compose.yml run --rm certbot
 docker exec nginx nginx -s reload
